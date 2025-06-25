f22d1e9dbca34f4dda06e5886c550029db045a88
Ian MacLeod <imacleod@mail.umw.edu>
2025-06-25 10:49:37 -0400

    Corrected loop and added plotting of the prices

--- main.py
+++ main.py
@@ -8,10 +8,19 @@ if __name__ == "__main__":
     prices = []
     base = 100
 
-    for i in range(len(100)):
-        trend = prices * .1
+    for i in range(100):
+        trend = i * .1
         noise = np.random.normal(0, 2)
         price = base + trend + noise
         prices.append(price)
     
+    plt.figure(figsize=(12, 8))
+    plt.xlabel("Time")
+    plt.ylabel("Price")
+    plt.title("Moving Average Strategy")
+    plt.plot(prices, label="Price")
+
+    plt.legend()
+
+    plt.show()
     
